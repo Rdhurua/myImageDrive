2d1d35919bafad401c1dbf01809ca4f96c42c6ad
Rinku Dhurua <rinkudhurua258@gmail.com>
2025-08-30 19:14:59 +0530

    routing issued resolved

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -13,7 +13,7 @@ const SignUp = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("http://localhost:4000/api/auth/signup", form);
+      await axios.post(`${import.meta.env.VITE_BACKEND_URL}/auth/signup`, form);
       navigate("/login");
     } catch (error) {
       alert(error.response?.data?.message || "Signup failed");
