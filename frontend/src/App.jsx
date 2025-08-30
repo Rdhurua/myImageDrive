eec22b4ab03ef29c738dcd8e7b217af93f2bdcca
Rinku Dhurua <rinkudhurua258@gmail.com>
2025-08-30 18:05:23 +0530

    final debug

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import { Routes, Route } from "react-router-dom";
-import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard"; // Will create later
 import Home from "./pages/Home";
+import SignUp from "./pages/Signup";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/signup" element={<Signup />} />
+      <Route path="/signup" element={<SignUp />} />
       <Route path="/login" element={<Login />} />
       <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
